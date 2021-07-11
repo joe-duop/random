ac62e38f186bb8d73b73063e44c88adee0c0d637
joe-duop <johannezwatulo@outlook.com>
2021-07-11 15:22:56 +0300

    created category views and templates

--- swahili/urls.py
+++ swahili/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.homeView, name="home"),
-
-    # path('cat', views.kiswahiliCategoryView, name="cat"),
-    # path('cat/<slug:the_slug>/', views.kiswahiliCategoryDetailView, name='cat_detail'),
-    #
-    # path('category', views.categoryView, name="category"),
-    # path('category/<slug:slug>/', views.categoryDetailView, name='category_detail'),
-    #
-    # path('sarufi', views.sarufiView, name="sarufi"),
-    # path('sarufi/<slug:slug_text>', views.sarufiDetail, name='sarufi_detail'),
+    #main_category
+    path('category', views.swahiliCategoryView, name="category"),
+    path('category/<slug:the_slug>/', views.swahiliCategoryDetailView, name='category_detail'),
+    #sub_category
+    path('sub_cat', views.swahiliSubCategoryView, name="sub_category"),
+    path('sub_cat/<slug:slug>/', views.swahiliSubCategoryDetailView, name='sub_category_detail'),
+    #content
+    path('topic', views.swahiliView, name="swahili"),
+    path('topic/<slug:slug_text>', views.swahiliDetailView, name='swahili_detail'),
 ]
--- swahili/views.py
+++ swahili/views.py
@@ -1,6 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 # Create your views here.
 def homeView(request):
     return render(request, 'home.html')
+
+
+#main category
+def swahiliCategoryView(request):
+    """lists all the categories created"""
+    sw_main = SwahiliCategory.objects.all()
+    context = {'sw_main':sw_main}
+    return render(request, 'category/swahiliCategoryView.html', context)
+
+def swahiliCategoryDetailView(request, the_slug):
+    """shows the details of a specific category - the sub-categories"""
+    sw_main_det = SwahiliCategory.objects.get(slug=the_slug)
+    context = {'sw_main_det':sw_main_det}
+    return render(request, 'category/swahiliCategoryDetailView.html', context)
+
+
+# sub-category
+def swahiliSubCategoryView(request):
+    """lists all the swahili-sub-categories created"""
+    sw_sub = SwahiliSubCategory.objects.all()
+    context = {'sw_sub':sw_sub}
+    return render(request, 'sub_category/swahiliSubCategoryView.html', context)
+
+def swahiliSubCategoryDetailView(request, slug):
+    """shows the details of a specific swahili-sub-category - the contents"""
+    sw_sub_det = SwahiliSubCategory.objects.get(slug=slug)
+    context = {'sw_sub_det':sw_sub_det}
+    return render(request, 'sub_category/swahiliSubCategoryDetailView.html', context)
+
+
+# content
+def swahiliView(request):
+    """lists all the topics created"""
+    swahili = SwahiliContent.objects.all()
+    context = {'swahili':swahili}
+    return render(request, 'topic/swahiliView.html', context)
+
+def swahiliDetailView(request, slug_text):
+    """shows the details of a specific topic - the contents"""
+    swahili_det = get_object_or_404(SwahiliContent, slug=slug_text)
+    context = {'swahili_det':swahili_det}
+    return render(request, 'topic/swahiliDetailView.html', context)
